89d8be5068d5b0608c62c9f7a27c02cd9f392f35
Gennadya <heavyyrain3039@gmail.com>
2019-07-11 17:09:20 +0300

    перенос шаблонов, активные ссылки

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {AuthModule} from './auth/auth.module';
 import {HttpClientModule} from '@angular/common/http';
 import {UserService} from './shared/service/user.service';
 import {AuthService} from './shared/service/auth.service';
+import {SystemModule} from './system/system.module';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import {AuthService} from './shared/service/auth.service';
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
-        AuthModule
+        AuthModule,
+        SystemModule
     ],
     providers: [UserService, AuthService],
     bootstrap: [AppComponent]
