d5ceed3f81666fdde5c92d4b27249f31e1502ae8
Gennadya <heavyyrain3039@gmail.com>
2019-07-16 16:34:03 +0300

    страница записей/ добавление событий / кнопки

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+
+import {CategoryModel} from '../../shared/models/category.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-add-event',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventComponent implements OnInit {
 
+  @Input() categories: CategoryModel[];
+  types = [
+    {type: 'income', label: 'Доход'},
+    {type: 'outcome', label: 'Расход'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  onSubmit(form: NgForm) {}
 
 }
